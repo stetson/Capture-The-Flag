80741b21da0e32197d3f7b1c8d5737e3af5e7d4b
Mark Cahill <mark@tiemonster.info>
2011-02-12 17:01:18 -0500

    Generate random UUID instead of worrying about Facebook Connect for the first iteration

--- frontend/js/application.js
+++ frontend/js/application.js
@@ -51,6 +51,15 @@ var map = {
  * @namespace model
  */
 var model = {
+		
+	/**
+	 * Facebook Connect authentication token
+	 * FIXME - this is just a random UUID at the moment
+	 */
+	auth_token: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+	    return v.toString(16);
+	}).toUpperCase(),
 	
 	/**
 	 * Conditions have been met for gameplay
