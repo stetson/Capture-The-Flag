3608fcdf24c0c2afdd873b62b26b32e3cce47141
Mwolodko <mwolodko@stetson.edu>
2011-02-15 15:08:02 -0800

    Edited frontend/js/application.js via GitHub

--- frontend/js/application.js
+++ frontend/js/application.js
@@ -54,7 +54,7 @@ var model = {
 	 * Facebook Connect authentication token
 	 * FIXME - this is just a random UUID at the moment
 	 */
-	auth_token: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	auth_token: 151829711542674.replace(/[xy]/g, function(c) {
 	    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 	    return v.toString(16);
 	}).toUpperCase(),
