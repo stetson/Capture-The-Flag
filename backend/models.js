8f314cf5c52b74c18578948dc7aa150bbe468f28
Mark Cahill <mark@tiemonster.info>
2011-02-17 20:36:26 -0500

    Cleaned up models

--- backend/models.js
+++ backend/models.js
@@ -13,36 +13,25 @@
 var settings = require("../settings.js");
 
 /**
-* The Node.js MongoDB adapter
-*/
+ * The Node.js MongoDB adapter
+ */
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 mongoose.connect(settings.mongodb_url);
-var SchemaType = require('../schematype')
-  , CastError = SchemaType.CastError;
-
-
 
 
 /**
-* Player object, which holds all information related to the player
-*
-* @memberOf models
-* @name Player
-*/
-
-var Player = new Schema({
-    name     : { type: String, validate: /[a-z]/ }
-  , team     : [Team]
-  , user_id  : {type: String}
-});
-
+ * Player object, which holds all information related to the player
+ *
+ * @memberOf models
+ * @name Player
+ */
 
 exports.Player = new Schema({
-
+	id: ObjectId,
+	name: { type: String, validate: /[a-z]/ },
+	user_id: {type: String }
 });
-
-
 
 /**
 * Game object, which holds information regarding boundaries
@@ -52,42 +41,22 @@ exports.Player = new Schema({
 * @name Game
 */
 
-var Game = new Schema({
-    player     :  [Player]
-  , team       :  [Team]
-  , coordinate :  [Coordinate]
- 
-});
-
-
 exports.Game = new Schema({
-
+	id: ObjectId,
+    teams: [Team],
+    initial_location: [Coordinate]
 });
-
-
 
 /**
-* Team object, used for proper load balancing of players
-* on teams
-*
-* @memberOf models
-* @name Team
-*/
-
-
-var Team = new Schema({
-    name  :  { type: String, validate: /[a-z]/ }
- 
-});
-
-
-
+ * Team object, used for proper load balancing of players
+ * on teams
+ *
+ * @memberOf models
+ * @name Team
+ */
 exports.Team = new Schema({
-
+    players: [Player] 
 });
-
-
-
 
 /**
 * Coordinate object, which holds a GPS coordinate,
@@ -96,14 +65,8 @@ exports.Team = new Schema({
 * @memberOf models
 * @name Coordinate
 */
-
-
-var Coordinate = new Schema({
-  name  :  { type: String, validate: /[a-z]/ }
- 
-});
-
-
 exports.Coordinate = new Schema({
-
+	latitude: Number,
+	longitude: Number,
+	accuracy: Number
 });
