f17a6fb014cb294150460b6990d92e83bdfd1ef7
Mark Cahill <mark@tiemonster.info>
2011-02-18 20:28:57 -0500

    Rearchitected to work with new design

--- backend/views.js
+++ backend/views.js
@@ -14,47 +14,6 @@
  */
 var models = require("./models.js");
 
-//Global memory caches
-
-/**
- * Stores all active players
- */
-var players;
-players = {};
-//setInterval(persist_players, 60000);
-
-/**
- * Purge old players
- * 
- * @memberOf players
- * @name purge_players
- */
-purge_players = function() {
-	for (var player_iterator in players) {
-		if (players.hasOwnProperty(player_iterator)) {
-			// Purge players who haven't updated in over 15 seconds
-			if (new Date() - players[player_iterator].last_update > 15000) {
-				players[player_iterator].latitude = 0;
-				players[player_iterator].longitude = 0;
-				players[player_iterator].accuracy = 0;
-			}
-			
-			// Reclaim memory of players who haven't updated in 5 minutes
-			if (new Date() - players[player_iterator].last_update > 300000) {
-				delete players[player_iterator];
-			}
-		}
-	}
-};
-
-setInterval(purge_players, 5000);
-
-/**
- * Stores all active games
- */
-var games = {};
-//setInterval(persist_games, 60000);
-
 /**
  * Update the user's location, and fetch the locations of
  * the other players
@@ -69,13 +28,13 @@ var games = {};
 exports.update_location = function(request, response) {	
 	// Record user's location
 	try {
-		players[request.body.user_id] = {
-			'latitude': request.body.latitude,
-			'longitude': request.body.longitude,
-			'accuracy': request.body.accuracy,
-			'last_update': new Date(),
-			'auth_token': request.body.auth_token
-		};
+		//players[request.body.user_id] = {
+		//	'latitude': request.body.latitude,
+		//	'longitude': request.body.longitude,
+		//	'accuracy': request.body.accuracy,
+		//	'last_update': new Date(),
+		//	'auth_token': request.body.auth_token
+		//};
 	} catch (e) { }
 	
 	// Send the players back to the client
