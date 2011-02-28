e7679ac6ade971d074039f569bcf43bd2eaedac0
Laura The Amazing <lseletos@stetson.edu>
2011-02-28 11:33:41 -0500

    Created the Algorithms.js file and I am writing the distance function

---
+++ backend/Algorithms.js
@@ -0,0 +1,29 @@
+//Algorithms.js File
+//Laura Seletos
+//Trying to follow the google tutorial on javascript api and adjust it to limiting a game's lat and long.
+
+
+//function that limits the map's latitude and longitude based on the user's gps corrdinates 
+function distance(user_latitude, user_longitude) {
+  
+  //creating new geo location limited to the user's lat and long
+  var playersLatLong = new google.maps.LatLng(user_latitude, user_longitude);
+
+  var myOptions = {
+    zoom: 8,
+    center: playersLatLong,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
+  
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.google.com/maps/api/js?sensor=false&callback=initialize";
+  document.body.appendChild(script);
+}
+  
+window.onload = loadScript;
+
