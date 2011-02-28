8713f26fcb7f89acfac68ec422d48e89a04b9ce9
jeremyy <jyanik@stetson>
2011-02-28 11:24:51 -0800

    Started implementation of in_rectangle() function.

--- modules/Algorithms.cc
+++ modules/Algorithms.cc
@@ -72,22 +72,55 @@ public:
   
     // Setting up the use of params
     HandleScope scope;
-	std::stringstream out;
+    std::stringstream out;
 	
-	// Grabbing our first two params (integers) and determining the type in c++
+    // Grabbing our first two params (integers) and determining the type in c++
     Local<Value> first = args[0];
-	Local<Value> second = args[1];
-	int a = first->Int32Value();
-	int b = second->Int32Value();
+    Local<Value> second = args[1];
+    int a = first->Int32Value();
+    int b = second->Int32Value();
 	
-	// Computations
-	int difference = a - b;
+    // Computations
+    int difference = a - b;
 	
-	// Returning the result
-	out << difference;
+    // Returning the result
+    out << difference;
     Local<String> result = String::New(out.str().c_str());
     return scope.Close(result);
   }
+
+  /*
+    (Not functioning at this time, needs more work.)
+    Determines if a coordinate lies within a rectangle
+	
+	Use:		in_rectangle(coord, coord(x1), coord(y1), coord(x2), coord(y2))
+	Return:		True if the first coordinate lies within the rectangle, otherwise false.
+	Notes:		The first coordinate is the one we wish to know about, while the later four are the rectangle.
+				This function assumes that the 4 coordinates provided are actually a rectangle or square!
+  */  
+  static Handle<Value> in_rectangle(const Arguments& args)
+  {
+  
+    // Setting scope and building an string stream for output
+    HandleScope scope;
+    std::stringstream out;
+	
+    // Grabbing function parameters
+    Local<Value> coord = args[0];
+    Local<Value> rectX1 = args[1];
+    Local<Value> rectY1 = args[2];
+    Local<Value> rectX2 = args[3];
+    Local<Value> rectY2 = args[4];
+	
+    // Computations
+    // ...
+	
+    // Returning the result
+    Local<String> result = String::New(out.str().c_str());
+    return scope.Close(result);
+  }
+
+  
 };
 
 Persistent<FunctionTemplate> Algorithms::s_ct;
