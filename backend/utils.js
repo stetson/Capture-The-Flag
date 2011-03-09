89fcd72e567e3e5ac49b45196892275a636d6929
Mark Cahill <mark@tiemonster.info>
2011-03-09 16:39:14 -0500

    Fixed purge methods

--- backend/utils.js
+++ backend/utils.js
@@ -9,18 +9,21 @@ exports.purge_players = function() {
         for (var player_iterator in ctf.game_data[game_iterator].players) {
             if (ctf.game_data[game_iterator].players.hasOwnProperty(player_iterator)) {
                 // Calculate time since last update
-                var time_since_last_update = new Date() - ctf.game_data[game_iterator].players[player_iterator].last_update;
+                var time_since_last_update = new Date() - new Date(ctf.game_data[game_iterator].players[player_iterator].last_update);
                 
                 // Purge players who haven't updated in over 1 minute
                 if (time_since_last_update >= ctf.constants.DISABLE_USER_INTERVAL * ctf.constants.MINUTE) {
                     ctf.game_data[game_iterator].players[player_iterator].latitude = 0;
                     ctf.game_data[game_iterator].players[player_iterator].longitude = 0;
                     ctf.game_data[game_iterator].players[player_iterator].accuracy = 0;
+                    console.log("Deactivating " + player_iterator);
                 }
                 
                 // Reclaim memory of players who haven't updated in 5 minutes
-                if (time_since_last_update >= ctf.constants.PURGE_USER_INTERVAL * ctf.constants.MINUTE) {
+                if (time_since_last_update >= ctf.constants.PURGE_USER_INTERVAL * ctf.constants.MINUTE ||
+                        ctf.game_data[game_iterator].players[player_iterator].last_update === undefined) {
                     delete ctf.game_data[game_iterator].players[player_iterator];
+                    console.log("Purging " + player_iterator);
                 }
             }
         }
@@ -32,11 +35,12 @@ exports.purge_games = function() {
     for (var game_iterator in ctf.game_data) {
         if (ctf.game_data.hasOwnProperty(game_iterator)) {
             // Calculate time since last update
-            var time_since_last_update = new Date() - ctf.game_data[game_iterator].last_update;
+            var time_since_last_update = new Date() - new Date(ctf.game_data[game_iterator].last_update);
             
             // Delete games that haven't been played on in a while
             if (time_since_last_update >= ctf.constants.PURGE_GAMES_INTERVAL * ctf.constants.MINUTE) {
                 delete ctf.game_data[game_iterator];
+                console.log("Purging " + game_iterator);
             }
         }
     }
