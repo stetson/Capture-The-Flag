7d272bbb54ad3adb4247f8129ce122d18f56757c
jeremyy <jyanik@stetson>
2011-03-21 05:18:13 -0700

    Added missing Algoriths.h file. (It is unclear why this file was removed.)

---
+++ modules/Algorithms.h
@@ -0,0 +1,35 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+// Earth's radius in Kilometers
+#define EARTH_RADIUS 6371.009
+
+// The estimated value of PI
+#define PI 3.1415926535
+
+// Miles to kilometer conversion factor
+#define MILES_PER_KILOMETER 0.621371192
+
+#include <sstream>
+#include <math.h>
+#include <vector>
+
+struct coord {
+  double latitude, longitude;
+};
+
+class Algorithms
+{
+
+  private:
+  
+  public:
+    static double toRad(double degrees);
+    static double toDeg(double radians);
+    static double distance_in_miles(double p1_latitude, double p1_longitude, double p2_latitude, double p2_longitude);
+    static bool in_rectangle(double findLat, double findLong, double topLeftLat, double topLeftLong, double botRightLat, double botRightLong);
+    static coord add_miles_to_coordinate(double latitude, double longitude, double offset, double bearing);
+
+};
+
+#endif
