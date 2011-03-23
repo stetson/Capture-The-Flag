c9d93f14e080477d48a58c4b398020ba652682b8
Mark Cahill <mark@tiemonster.info>
2011-03-23 10:32:23 -0400

    Simplified testing of bounds. Created a structure that allows for testing a bunch of points really easily.

--- test/test-bounds.js
+++ test/test-bounds.js
@@ -17,50 +17,47 @@ var user = {
 };
 
 exports.test_bounds = function(test) {
-    // TODO - create game (see test-game-workflow.js)  
-controller.create_game(game_id, user.latitude, user.longitude)
-    // game = 
-game = controller.create_game(game_id, user.latitude, user.longitude)
-    // TODO - generate points around where bounds are using http://www.movable-type.co.uk/scripts/latlong.html
-    // TODO - test to see if in_rectangle returns the results we expect (see test-algorithms.js)
+    // Create game (see test-game-workflow.js)
+    controller.create_game(game_id, user.latitude, user.longitude);
+    var game = ctf.game_data[game_id];
     
-    // You'll be doing this a lot:
-    // point_from_site = {
-    //     latitude: 29.034681
-    //     longitude: -81.303774
-    // }
+    // The different boundaries to test against
+    var boundaries = { "red": 0, "blue": 1, "field": 2 };
 
-
-var point_top_left = algorithms.add_miles_to_coordinate(user.latitude, user.longitude, Math.sqrt(0.5), 315);
-
-var point_bottom_right = algorithms.add_miles_to_coordinate(user.latitude, user.longitude, 0.5, 90);
-
-var point_top_right = algorithms.add_miles_to_coordinate(point_bottom_right.latitude, point_bottom_right.longitude, 0.5, 0);
-
-var distance = algorithms.distance_in_miles( point_top_right.latitude, point_top_right.longitude,point_bottom_right.latitude,point_bottom_right.longitude);
-var distance1 = algorithms.distance_in_miles( point_top_left.latitude, point_top_left.longitude,point_top_right.latitude,point_top_right.longitude);
-
-
-var bool = (distance < 0.51);
-var bool1 = (distance1 < 1.0);
-
-test.strictEqual(true, bool,"distance messed up");
-test.strictEqual(true, bool1,"distance messed up");
-
-//Testing distance
-console.log(" distance\n -> " + distance1+ " miles");
-
-    // For points that should be in the rectangle:
-    // test.ok(algorithms.in_rectangle(ctf.game_data[game_id].red_bounds.latitude, ...
+    // Generate points around where bounds are using http://www.movable-type.co.uk/scripts/latlong.html
+    // Do *NOT* use the Algorithms class to generate these points
+    var points_to_test = [
+        // Test mid-field boundary line
+        { latitude: user.latitude, longitude: game.red_bounds.bottom_right.longitude, expected_result: true, boundary: boundaries.red },
+        { latitude: user.latitude, longitude: game.red_bounds.top_left.longitude, expected_result: true, boundary: boundaries.red }
+        
+        // TODO - test corners
+        // TODO - test points known to be inside
+        // TODO - test points known to be outside
+        // TODO - test points very close to where the line should be on either side
+    ];
     
-    // For points that should NOT be in the rectangle:
-    // test.ok(! algorithms.in_rectangle(ctf.game_data[game_id].red_bounds.latitude, ...
-    
-    // TODO - test corners
-    // TODO - test mid-field boundary line
-    // TODO - test points known to be inside
-    // TODO - test points known to be outside
-    // TODO - test points very close to where the line should be on either side
-    
+    // Test to see if points are in the rectangle, and match against expected result:
+    // (see test-algorithms.js for examples of usage of in_rectangle)
+    for (var i = 0; i < points_to_test.length; i++) {
+        switch (points_to_test[i].boundary) {
+        case boundaries.red:
+            test.strictEqual(points_to_test[i].expected_result, algorithms.in_rectangle(points_to_test[i].latitude, points_to_test[i].longitude,
+                    game.red_bounds.top_left.latitude, game.red_bounds.top_left.longitude,
+                    game.red_bounds.bottom_right.latitude, game.red_bounds.bottom_right.longitude));
+            break;
+        case boundaries.blue:
+            test.strictEqual(points_to_test[i].expected_result, algorithms.in_rectangle(points_to_test[i].latitude, points_to_test[i].longitude,
+                    game.blue_bounds.top_left.latitude, game.blue_bounds.top_left.longitude,
+                    game.blue_bounds.bottom_right.latitude, game.blue_bounds.bottom_right.longitude));
+            break;
+        case boundaries.field:
+            test.strictEqual(points_to_test[i].expected_result, algorithms.in_rectangle(points_to_test[i].latitude, points_to_test[i].longitude,
+                    game.red_bounds.top_left.latitude, game.red_bounds.top_left.longitude,
+                    game.blue_bounds.bottom_right.latitude, game.blue_bounds.bottom_right.longitude));
+            break;
+        }        
+    }
+
     test.done();
 };
