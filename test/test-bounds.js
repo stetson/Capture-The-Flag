8a1d29277e7a6c634debbaddbb21bb45bc23cf3f
Mark Cahill <mark@tiemonster.info>
2011-03-23 10:58:48 -0400

    Added a bit more documentation. Finished testing mid-field boundary line.

--- test/test-bounds.js
+++ test/test-bounds.js
@@ -27,9 +27,15 @@ exports.test_bounds = function(test) {
     // Generate points around where bounds are using http://www.movable-type.co.uk/scripts/latlong.html
     // Do *NOT* use the Algorithms class to generate these points
     var points_to_test = [
-        // Test mid-field boundary line
-        { latitude: user.latitude, longitude: game.red_bounds.bottom_right.longitude, expected_result: true, boundary: boundaries.red },
-        { latitude: user.latitude, longitude: game.red_bounds.top_left.longitude, expected_result: true, boundary: boundaries.red }
+        // Test mid-field boundary line (Points right on the line should be in red territory)
+        { latitude: user.latitude, longitude: game.red_bounds.bottom_right.longitude, boundary: boundaries.red, expected_result: true },
+        { latitude: user.latitude, longitude: game.red_bounds.top_left.longitude, boundary: boundaries.red, expected_result: true },
+        { latitude: user.latitude, longitude: game.blue_bounds.bottom_right.longitude, boundary: boundaries.blue, expected_result: false },
+        { latitude: user.latitude, longitude: game.blue_bounds.top_left.longitude, boundary: boundaries.blue, expected_result: false },
+        
+        // Test just a hair to the South (Should be in blue territory)
+        { latitude: user.latitude - 0.000001, longitude: game.blue_bounds.bottom_right.longitude, boundary: boundaries.blue, expected_result: true },
+        { latitude: user.latitude - 0.000001, longitude: game.blue_bounds.top_left.longitude, boundary: boundaries.blue, expected_result: true }
         
         // TODO - test corners
         // TODO - test points known to be inside
@@ -42,16 +48,19 @@ exports.test_bounds = function(test) {
     for (var i = 0; i < points_to_test.length; i++) {
         switch (points_to_test[i].boundary) {
         case boundaries.red:
+            // Test to see if the point is in the red territory
             test.strictEqual(points_to_test[i].expected_result, algorithms.in_rectangle(points_to_test[i].latitude, points_to_test[i].longitude,
                     game.red_bounds.top_left.latitude, game.red_bounds.top_left.longitude,
                     game.red_bounds.bottom_right.latitude, game.red_bounds.bottom_right.longitude));
             break;
         case boundaries.blue:
+            // Test to see if the point is in the blue territory
             test.strictEqual(points_to_test[i].expected_result, algorithms.in_rectangle(points_to_test[i].latitude, points_to_test[i].longitude,
                     game.blue_bounds.top_left.latitude, game.blue_bounds.top_left.longitude,
                     game.blue_bounds.bottom_right.latitude, game.blue_bounds.bottom_right.longitude));
             break;
         case boundaries.field:
+            // Test to see if the point is on the field
             test.strictEqual(points_to_test[i].expected_result, algorithms.in_rectangle(points_to_test[i].latitude, points_to_test[i].longitude,
                     game.red_bounds.top_left.latitude, game.red_bounds.top_left.longitude,
                     game.blue_bounds.bottom_right.latitude, game.blue_bounds.bottom_right.longitude));
