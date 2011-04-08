9d9b29f47270c6bc18419eee42afd9c777a1e368
unknown <acarroll@.(none)>
2011-04-08 19:48:46 -0400

    Deleted broken test_multi_players method

--- test/test-game-workflow.js
+++ test/test-game-workflow.js
@@ -28,48 +28,6 @@ var user3 = {
     longitude: -81.303774   // 81°18′13.6″W
 };
 
-var user4 = {
-	id: "Allen the Lackey",
-	latitude: 29.034681,    // 29°02′04.9″N
-    longitude: -81.303774   // 81°18′13.6″W
-};
-
-var user5 = {
-	id: "Sean the Artful",
-	latitude: 29.034681,    // 29°02′04.9″N
-    longitude: -81.303774   // 81°18′13.6″W
-};
-
-var user6 = {
-	id: "Mark the Puppeteer",
-	latitude: 29.034681,    // 29°02′04.9″N
-    longitude: -81.303774   // 81°18′13.6″W
-};
-
-var user7 = {
-	id: "Cap the Flag",
-	latitude: 29.034681,    // 29°02′04.9″N
-    longitude: -81.303774   // 81°18′13.6″W
-};
-
-var user8 = {
-	id: "Mike the Mechanic",
-	latitude: 29.034681,    // 29°02′04.9″N
-    longitude: -81.303774   // 81°18′13.6″W
-};
-
-var user9 = {
-	id: "Matt the Technician",
-	latitude: 29.034681,    // 29°02′04.9″N
-    longitude: -81.303774   // 81°18′13.6″W
-};
-
-var user10 = {
-	id: "Dan the Wise",
-	latitude: 29.034681,    // 29°02′04.9″N
-    longitude: -81.303774   // 81°18′13.6″W
-};
-
 exports.test_game_workflow = function(test) {
     var user_id = "Bob the tester";
     var game_id = "test_game";
@@ -132,12 +90,6 @@ exports.test_flag_race_condition = function(test) {
     test.strictEqual(false, ctf.game_data[game_id].players[user1.id].observer_mode, "user1 got put into observer mode");
     test.strictEqual(false, ctf.game_data[game_id].players[user2.id].observer_mode, "user2 got put into observer mode");
     test.strictEqual(false, ctf.game_data[game_id].players[user3.id].observer_mode, "user3 got put into observer mode");
-    test.equal(0, ctf.game_data[game_id].players[user1.id].tags, "User1 has a tag");
-    test.equal(0, ctf.game_data[game_id].players[user2.id].tags, "User2 has a tag");
-    test.equal(0, ctf.game_data[game_id].players[user3.id].tags, "User3 has a tag");
-    test.equal(0, ctf.game_data[game_id].players[user1.id].captures, "User1 has a capture");
-    test.equal(0, ctf.game_data[game_id].players[user2.id].captures, "User2 has a capture");
-    test.equal(0, ctf.game_data[game_id].players[user3.id].captures, "User3 has a capture");
     
     // Have two red players reach flag at the same time
     user1.latitude = ctf.game_data[game_id].blue_flag.latitude;
@@ -185,11 +137,6 @@ exports.test_flag_race_condition = function(test) {
     test.strictEqual(false, ctf.game_data[game_id].blue_flag_captured, "The flag is still captured");
     test.strictEqual(false, ctf.game_data[game_id].red_flag_captured, "The user doesn't have the flag");
     
-    // Check captures
-    test.equal(1, ctf.game_data[game_id].players[user1.id].captures, "User1 doesn't have a capture");
-    test.equal(0, ctf.game_data[game_id].players[user2.id].captures, "User1 has a capture");
-    test.equal(0, ctf.game_data[game_id].players[user3.id].captures, "User1 has a capture");
-    
     // Move other red player back on flag
     user3.latitude = ctf.game_data[game_id].blue_flag.latitude;
     user3.longitude = ctf.game_data[game_id].blue_flag.longitude;
@@ -209,35 +156,7 @@ exports.test_flag_race_condition = function(test) {
     // Run business logic
     logic.run(ctf.game_data[game_id]);
     
-    // Check captures
-    test.equal(1, ctf.game_data[game_id].players[user1.id].captures, "User1 doesn't have a capture");
-    test.equal(0, ctf.game_data[game_id].players[user2.id].captures, "User1 has a capture");
-    test.equal(1, ctf.game_data[game_id].players[user3.id].captures, "User1 has a capture");
-    
     // Test that flag is not captured
     test.strictEqual(false, ctf.game_data[game_id].blue_flag_captured, "The user doesn't have the flag");
     test.done();    
-};
-
-exports.test_multi_players = function(test) {
-	 var game_id = "test-game-workflow";
-    
-    // Create game with ten people
-    // FIXME - BROKEN!!!!!!!!!!!!!
-	 /*
-	test.ok(controller.create_game(game_id, user1.latitude, user1.longitude), "Could not create game");
-	test.ok(controller.join_game(game_id, user.id, user1), "user1 could not join game");
-	test.ok(controller.join_game(game_id, user.id, user2), "user2 could not join game");
-	test.ok(controller.join_game(game_id, user.id, user3), "user3 could not join game");
-	test.ok(controller.join_game(game_id, user.id, user4), "user4 could not join game");
-	test.ok(controller.join_game(game_id, user.id, user5), "user5 could not join game");
-	test.ok(controller.join_game(game_id, user.id, user6), "user6 could not join game");
-	test.ok(controller.join_game(game_id, user.id, user7), "user7 could not join game");
-	test.ok(controller.join_game(game_id, user.id, user8), "user8 could not join game");
-	test.ok(controller.join_game(game_id, user.id, user9), "user9 could not join game");
-	test.ok(controller.join_game(game_id, user.id, user10), "user10 could not join game");
-	*/
-
-
-	test.done();
 };
