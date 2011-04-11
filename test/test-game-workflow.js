5fb5c1046a3b41b3010491eb510993765f4f3d88
unknown <acarroll@.(none)>
2011-04-11 09:07:42 -0400

    Outlined one test case for testing if tagging in observor mode occurs and another for testing if the flag can be gained while in observor mode

--- test/test-game-workflow.js
+++ test/test-game-workflow.js
@@ -338,3 +338,70 @@ exports.test_game_workflow = function(test) {
 
     test.done();    
 };
+/*
+exports.test_observer_tagging = function(test) {
+	var user_id = "Red1";
+    var game_id = "test_game";
+    var user = {
+        latitude: 29.034681,
+        longitude: -81.303774     
+    };
+	
+	var user_id = "Blue1";
+    var game_id = "test_game";
+    var user = {
+        latitude: 29.034681,
+        longitude: -81.303774     
+    };
+	
+	var user_id = "Red2";
+    var game_id = "test_game";
+    var user = {
+        latitude: 29.034681,
+        longitude: -81.303774     
+    };
+	
+	//Move Red1 to blue flag area
+	
+	//Check to see if Red1 has flag
+	
+	//Move Blue1 to outside field but within tagging range of Red1
+	
+	//Update logic
+	
+	//Check that Blue1 is in observer mode
+	
+	//Check to see if Red1 is still active and has flag
+
+test.done();
+};
+
+exports.test_tagging = function(test) {
+	var user_id = "Red1";
+    var game_id = "test_game";
+    var user = {
+        latitude: 29.034681,
+        longitude: -81.303774     
+    };
+	
+	var user_id = "Blue1";
+    var game_id = "test_game";
+    var user = {
+        latitude: 29.034681,
+        longitude: -81.303774    
+	};
+		//Move Red1 to blue flag area
+		
+		//Check to see if Red1 has flag
+		
+		//Move Blue1 to tagging distance of Red1 within field
+		
+		//Update logic
+		
+		//Check to see if Red1 is in observer mode
+		
+		//Check to see if Red1 has the flag
+    
+
+test.done();
+};*/
